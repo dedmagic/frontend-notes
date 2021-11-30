5ae8370f8ee43d3bc83830e2aefe2f390daff7fd
Олег Ищенко <ishchenko@parma.ru>
2021-11-30 12:00:55 +0500

    rename folder 'symbol'

---
+++ examples/experiments.js
@@ -0,0 +1,10 @@
+const prop_name = Symbol('prop_name');
+
+const obj = {
+    prop_s: 'Hello',
+    prop_n: 42,
+    [prop_name]: 96,
+}
+
+const json = JSON.stringify(obj, null, '  ');
+console.log(json);
