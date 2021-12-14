14ffc28d866879b09af02d501bb6b1550c6250e3
Олег Ищенко <ishchenko@parma.ru>
2021-12-14 17:06:05 +0500

    transfer examples from replit finished

--- examples/experiments.js
+++ examples/experiments.js
@@ -1,14 +1,5 @@
-const prop_name = Symbol('prop_name');
+let jsonStr = '{ "Id": 1, "IsTransferToService": true, "CurrentYearColumnTitle": "Целевые статьи статьи расходов бюджета города Москвы на 2021 год", "NextYearColumnTitle": "Целевые статьи расходов бюджета города Москвы на 2022 год", "CsrMatchingTableRows": [ { "Id": 10, "CurrentYearCsr": { "Id": 11, "Code": "123", "Name": "Наименование ЦСР1" }, "NextYearCsr": { "Id": 12, "Code": "456", "Name": "Наименование ЦСР2" }, "IsMoved": false }, { "Id": 20, "CurrentYearCsr": { "Id": 23, "Code": "789", "Name": "Наименование ЦСР3" }, "NextYearCsr": { "Name" : "Исключена." } "IsMoved": false }, { "Id": 30, "CurrentYearCsr": null, "NextYearCsr": { "Id": 34, "Code": "444", "Name": "Наименование ЦСР4" }, "IsMoved": false }, { "Id": 40, "CurrentYearCsr": { "Id": 45, "Code": "777", "Name": "Наименование ЦСР5" }, "NextYearCsr": { "Id": 46, "Code": "444", "Name": "Перемещена в 444." }, "IsMoved": true } ]}';
+//let jsonStr = '{ "Id": 1, "IsTransferToService": true, "CurrentYearColumnTitle": "Целевые статьи расходов бюджета города Москвы на 2021 год", "NextYearColumnTitle": "Целевые статьи расходов бюджета города Москвы на 2022 год", "CsrMatchingTableRows": [ { "Id": 10, "CurrentYearCsr": { "Id": 11, "Code": "123", "Name": "Наименование ЦСР1" }, "NextYearCsr": { "Id": 12, "Code": "456", "Name": "Наименование ЦСР2" }, "IsMoved": false } ]}';
 
-const obj = {
-    prop_s: 'Hello',
-    prop_n: 42,
-    [prop_name]: 96,
-}
-
-const json = JSON.stringify(obj, null, '  ');
+let json = JSON.parse(jsonStr);
 console.log(json);
-
-
-let d = { count: 42 };
-console.log(d.length > 0);
