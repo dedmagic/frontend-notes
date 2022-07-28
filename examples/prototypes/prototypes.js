7f7bf45c4e45c4aa45c5d79e241c3749f605df19
dedmagic <dedmagic@gmail.com>
2022-07-28 07:18:44 +0500

    WIP

---
+++ examples/prototypes/prototypes.js
@@ -0,0 +1,29 @@
+const obj = {
+    a: 42,
+    b: 96,
+}
+
+console.log(obj.hasOwnProperty('b'));
+console.log(obj.toString());
+
+// console.log(obj.__proto__);
+// console.log(obj.__proto__.__proto__);
+
+// const Person = function (name, age) {
+//     this.name = name;
+//     this.age = age;
+// }
+
+// const person = new Person('Mark Knopfler', 72);
+// console.log(person);
+
+// const objj = new Object();
+// const arr = new Array();
+// const str = new String();
+
+// console.log(objj, arr, str);
+
+// const arr = [];
+// console.log(arr.__proto__);
+// console.log(arr.__proto__.__proto__);
+// console.log(arr.__proto__.__proto__.__proto__);
