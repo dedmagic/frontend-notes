2bdf10443ca5acb04fcde1fde4c6ca664d0aa9d2
dedmagic <dedmagic@gmail.com>
2022-08-30 07:11:17 +0500

    note 'regexp' in progress

--- examples/regexp/03 string methods.js
+++ examples/regexp/03 string methods.js
@@ -1,23 +1,55 @@
-const shakespeare = 'To be, or not to be, that is the question!';
-const regexp = /on|no/gi;
+// const shakespeare = 'To be, or not to be, that is the question!';
+// const regexp = /on|no/gi;
 
-// String.prototype.search
-const res1 = shakespeare.search(regexp); // flag 'g' ignored
-console.log(res1); // 10
+// // String.prototype.search
+// const res1 = shakespeare.search(regexp); // flag 'g' ignored
+// console.log(res1); // 10
 
-const res2 = 'Hello world!'.search(regexp);
-console.log(res2); // -1
+// const res2 = 'Hello world!'.search(regexp);
+// console.log(res2); // -1
 
-const res3 = shakespeare.search(/to/i); // flag 'i' used
-console.log(res3); // 0
+// const res3 = shakespeare.search(/to/i); // flag 'i' used
+// console.log(res3); // 0
 
-// Параметр-строка преобразуется в объект RegExp
+// // Примечание: параметр-строка преобразуется в объект RegExp
 
-// String.prototype.replace
-const res4 = shakespeare.replace(regexp, '42');
-console.log(res4); // To be, or 42t to be, that is the questi42!
+// // String.prototype.replace
+// const res4 = shakespeare.replace(regexp, '42');
+// console.log(res4); // To be, or 42t to be, that is the questi42!
 
-const res5 = shakespeare.replace(/(to) (be)/gi, '$2 $1'); // Поменять слова местами
-console.log(res5); // be To, or not be to, that is the question!
+// const res5 = shakespeare.replace(/(to) (be)/gi, '$2 $1'); // Поменять слова местами
+// console.log(res5); // be To, or not be to, that is the question!
 
-// String.prototype.match
+// // String.prototype.match with 'g' flag
+// const res6 = shakespeare.match(regexp);
+// console.log(res6); // [ 'no', 'on' ]
+
+// String.prototype.match without 'g' flag
+const message = 'Write me to superman@yandex.com. By!';
+// const atRegExp = /@/;
+// const res7 = message.match(atRegExp);
+// console.log(res7);
+// /*
+// [
+//   '@',
+//   index: 20,
+//   input: 'Write me to superman@yandex.com. By!',
+//   groups: undefined
+// ]
+// */
+// console.log(res7[0]);
+
+// // String.prototype.match without 'g' flag and with groups
+// const emailRegExp = /(\w+)@(\w+)\.(\w+)/;
+// const res8 = message.match(emailRegExp);
+// console.log(res8[0]); // superman@yandex.com
+// console.log(res8[1]); // superman
+// console.log(res8[2]); // yandex
+// console.log(res8[3]); // com
+
+// String.prototype.match without 'g' flag and with naming groups
+const emailRegExpNamingGroups = /(?<user>\w+)@(?<domain2>\w+)\.(?<domain1>\w+)/;
+const res9 = message.match(emailRegExpNamingGroups);
+console.log(res9.groups.user); // superman
+console.log(res9.groups.domain2); // yandex
+console.log(res9.groups.domain1); // com
